a637857ca3c86bf4e40805fe906e7102b9fa29d5
Nazar <Gegeranazar06@gmail.com>
2022-09-06 14:36:09 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Carousel
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  // циклічність безкінечна прокрутка
+  loop: true,
+  // отсуп між ними
+  margin: 30,
+  startPosition: 1,
+  // 3 слайда за замовчуванням
+  items: 3,
+  responsive: {
+    0: {},
+    480: {},
+
+    1000: {
+      margin: 20,
+    },
+    1200: {
+      margin: 30,
+    },
+  },
+});
+
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
+
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+
+// nav icon
+
+var navBtn = document.querySelector(".nav__toggle");
+var nav = document.querySelector(".nav");
+var menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
+  menuIcon.classList.toggle("menu-icon--active");
+  document.body.classList.toggle("no-scroll");
+};
